cb87ee8597041683570550c0a49b780d00c8a0cc
Diego Hernandez Herrera <diegohh0411@gmail.com>
2025-10-22 14:39:25 -0600

    fix: remove everything in the original implementation

--- src/routes/categoria.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllCategorias } from '../controllers/categoria.controller.js';
-
-const router = express.Router();
-
-router.get('/categorias', getAllCategorias);
-
-export default router;
--- src/routes/contenido.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getContenidoByCategoria } from '../controllers/contenido.controller.js';
-
-const router = express.Router();
-
-router.get('/contenido/:categoriaId', getContenidoByCategoria);
-
-export default router;

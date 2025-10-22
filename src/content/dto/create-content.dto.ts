62695c88b4ec33c9d9cf74d87bdb970466312541
Diego Hernandez Herrera <diegohh0411@gmail.com>
2025-10-22 14:36:21 -0600

    feat: implement CreateContentDto with validation rules for content creation

--- src/content/dto/create-content.dto.ts
+++ src/content/dto/create-content.dto.ts
@@ -1,1 +1,19 @@
-export class CreateContentDto {}
+import { IsEnum, IsString } from "class-validator";
+import { ContentType } from "../entities/content.entity";
+
+export class CreateContentDto {
+    @IsString()
+    name: string;
+
+    @IsString()
+    details: string;
+
+    @IsEnum(ContentType)
+    type: ContentType;
+
+    @IsString()
+    url: string;
+
+    @IsString()
+    transcript: string;
+}

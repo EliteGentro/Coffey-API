665295b4ab4f0e2f0dde4b8a71d6c3cb5d152333
Diego Hernandez Herrera <diegohh0411@gmail.com>
2025-12-04 13:00:04 -0600

    feat: define CreateCooperativaDto for cooperative entity creation

--- src/cooperativa/dto/create-cooperativa.dto.ts
+++ src/cooperativa/dto/create-cooperativa.dto.ts
@@ -1,7 +1,19 @@
-import { IsString, IsNotEmpty } from "class-validator";
+import { IsString, IsNotEmpty, IsOptional } from "class-validator";
 
 export class CreateCooperativaDto {
     @IsString()
     @IsNotEmpty()
     name: string;
+
+    @IsString()
+    @IsOptional()
+    createdAt: Date;
+
+    @IsString()
+    @IsOptional()
+    updatedAt: Date;
+
+    @IsString()
+    @IsOptional()
+    deletedAt?: Date | null;
 }
